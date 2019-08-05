7695827aca9bd24d830ccdce5280fa361956c560
李庆松 <lqs@ecrrc.com>
2019-08-05 18:53:08 +0800

    路由异步加载

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,38 +1,17 @@
-/* export default [
-  { 
-    path: '/', 
-    name: 'IndexPage',
-    // layout: BaseLayout,
-    models: () => [],
-    component: () => import('./home/')
-   },
-   {
-     path: '/products',
-     name: 'product',
-     layout: BaseLayout,
-     models: () => [import('./products/models')],
-     component: () => import('./products/')
-   },
-   {
-    path: '/products/add',
-    name: 'product-add',
-    layout: BaseLayout,
-    models: () => [],
-    component: () => import('./products/add')
-  }
-]; */
+import dynamic from 'dva/dynamic';
 import BaseLayout from '@/components/BaseLayout/';
 import EmptyLayout from '@/components/EmptyLayout/';
 import Page404 from '@/routes/single-page/404';
-import Login from '@/routes/login/index';
-import HomeIndex from '@/routes/home/index';
-import UsersList from '@/routes/users/list';
-import UsersAdd from '@/routes/users/add';
+// import Login from '@/routes/login/index';
+// import HomeIndex from '@/routes/home/index';
+// import UsersList from '@/routes/users/list';
+// import UsersAdd from '@/routes/users/add';
 
 export default [
   {
     path: "/login",
-    component: Login
+    // component: Login
+    component: dynamic({ component: () => import('@/routes/login/index'), models:()=>[] })
   },
   {
     path: "/404",
@@ -45,7 +24,8 @@ export default [
     routes: [
       {
         path: "/base/home",
-        component: HomeIndex
+        // component: HomeIndex
+        component: dynamic({ component: () => import('@/routes/home/index'), models:()=>[] })
       },
       {
         path: "/base/users",
@@ -53,11 +33,13 @@ export default [
         routes: [
           {
             path: "/base/users/list",
-            component: UsersList
+            // component: UsersList
+            component: dynamic({ component: () => import('@/routes/users/list'), models:()=>[] })
           },
           {
             path: "/base/users/add",
-            component: UsersAdd
+            // component: UsersAdd
+            component: dynamic({ component: () => import('@/routes/users/add'), models:()=>[] })
           }
         ]
       }
