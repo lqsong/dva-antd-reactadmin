11c793e0c338088386cd89ffba97d0a8b16c7dee
李庆松 <lqs@ecrrc.com>
2019-08-06 09:46:38 +0800

    修改默认框架

--- src/components/BaseLayout/index.jsx
+++ src/components/BaseLayout/index.jsx
@@ -3,24 +3,43 @@ import {connect} from 'dva';
 import { Link } from 'dva/router';
 import { RouteWithSubRoutes } from '@/utils/core';
 
-import { Layout, Menu, Icon } from 'antd';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 import style from './index.less';
-const { Header, Sider, Content } = Layout;
+const { Header, Sider, Content, Footer } = Layout;
 
 class BaseLayout extends React.Component {
     state = {
-      collapsed: false,
+      collapsedWidth: 80,
+      siderWidth: 200,
+      collapsed: false
     };
     toggle = () => {
       this.setState({
         collapsed: !this.state.collapsed,
       });
     };
+
+    getHeadWidth = () => {
+      const { collapsedWidth, siderWidth, collapsed  } = this.state;
+      return collapsed ? `calc(100% - ${collapsedWidth}px)` : `calc(100% - ${siderWidth}px)`;
+    }
+
+    getContentPaddingLeft= () => {
+      const { collapsedWidth, siderWidth, collapsed  } = this.state;
+      return collapsed ? `${collapsedWidth}px` : `${siderWidth}px`;
+    }
   
     render() {
+      const width = this.getHeadWidth();
+      const paddingLeft = this.getContentPaddingLeft();
       return (
         <Layout id="lqs-base-layout">
-          <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+          <Sider trigger={null} width={this.state.siderWidth} collapsedWidth={this.state.collapsedWidth} collapsible collapsed={this.state.collapsed}  style={{
+              overflow: 'auto',
+              height: '100vh',
+              position: 'fixed',
+              left: 0,
+            }}>
             <div className={style.logo} />
             <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
               <Menu.Item key="1">
@@ -43,28 +62,26 @@ class BaseLayout extends React.Component {
               </Menu.Item>
             </Menu>
           </Sider>
-          <Layout>
-            <Header style={{ background: '#fff', padding: 0 }}>
+          <Layout style={{  minHeight: '100vh', paddingLeft }}>
+            <Header style={{ background: '#fff', padding: 0, zIndex: 2, width }} className={style.headerfixed}>
               <Icon
                 className={style.trigger}
                 type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
                 onClick={this.toggle}
               />
+              <Breadcrumb className={style.breadcrumb}>
+                <Breadcrumb.Item>User</Breadcrumb.Item>
+                <Breadcrumb.Item>Bill</Breadcrumb.Item>
+              </Breadcrumb>
             </Header>
-            <Content
-              style={{
-                margin: '24px 16px',
-                padding: 24,
-                background: '#fff',
-                minHeight: 280,
-              }}
-            >
-              {     
-              this.props.routes.map((route, i) => (
-                <RouteWithSubRoutes key={i} {...route} />
-              ))
-              }
+            <Content  style={{ padding: '88px 24px 24px' }}  >
+                {     
+                this.props.routes.map((route, i) => (
+                  <RouteWithSubRoutes key={i} {...route} />
+                ))
+                }
             </Content>
+            <Footer style={{ textAlign: 'center' }}>Dva Antd RecatAdmin ©2019 Created by WYXGN.COM</Footer>
           </Layout>
         </Layout>
       );
