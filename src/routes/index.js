f85bc900ab6725e7b6f065758280d4d324332ae5
李庆松 <lqs@ecrrc.com>
2019-08-15 18:43:28 +0800

    菜单-meta

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,15 @@
  * @weburl http://www.liqingsong.cc 
  *         http://www.wyxgn.com
  */
+/**
+ * // meta可配置的参数:
+ * meta: {
+ *  title: { String }
+ *  icon: (-) 该页面在左侧菜单显示的图标
+ *  hideInMenu: (false) 设为true后在左侧菜单不会显示该页面选项
+ *  access: (null) 可访问该页面的权限数组，当前路由设置的权限会影响子路由
+ * }
+ */
 import dynamic from 'dva/dynamic';
 import BaseLayout from '@/components/BaseLayout/';
 import EmptyLayout from '@/components/EmptyLayout/';
@@ -32,35 +41,68 @@ export default (app) => {
           routes: [
             {
               path: "/base/home",
-              // component: HomeIndex
+              meta: {                
+                title: '首页',
+                icon: 'user',
+                hideInMenu: false,                
+                access: ['admin']                
+              },
               component: appBool ? dynamic({app, component: () => import('@/routes/home/index'), models:()=>[] }) : ''
             },
             {
               path: "/base/users",
+              meta: {                
+                title: '用户样例',
+                icon: 'user',
+                hideInMenu: false,                
+                access: ['admin']                
+              },
               component: appBool ? EmptyLayout : '',
               routes: [
                 {
                   path: "/base/users/list",
-                  // component: UsersList
+                  meta: {                
+                    title: '用户列表',
+                    hideInMenu: false,                
+                    access: ['admin']                
+                  },
                   component: appBool ? dynamic({app, component: () => import('@/routes/users/list'), models:()=>[import('@/routes/users/models/index')] }) : ''
                 },
                 {
                   path: "/base/users/add",
-                  // component: UsersAdd
+                  meta: {                
+                    title: '用户添加',
+                    hideInMenu: false,                
+                    access: ['admin']                
+                  },
                   component: appBool ? dynamic({app, component: () => import('@/routes/users/add'), models:()=>[] }) : ''
                 }
               ]
             },
             {
               path: "/base/products",
+              meta: {                
+                title: '产品样例',
+                icon: 'user',
+                hideInMenu: false,                
+                access: ['admin']                
+              },
               component: appBool ? EmptyLayout : '',
               routes: [
                 {
                   path: "/base/products/list",
+                  meta: {                
+                    title: '产品列表',
+                    hideInMenu: false,                
+                    access: ['admin']                
+                  },
                   component: appBool ? dynamic({app, component: () => import('@/routes/products/index'), models:()=>[import('@/routes/products/models/index')] }) : ''
                 }
               ]
             }
+
+
+            
 
           ]
         }
